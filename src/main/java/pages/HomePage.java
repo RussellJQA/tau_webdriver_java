3cf9546a8b85a024ba225925e3fa7c2e580d2204
Russell Johnson <16086475+RussellJQA@users.noreply.github.com>
2021-05-29 17:33:00 -0400

    Add chapter 5 example

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -5,26 +5,31 @@ import org.openqa.selenium.WebDriver;
 
 public class HomePage {
 
-        private WebDriver driver;
+    private WebDriver driver;
 
-        public HomePage(WebDriver driver) { this.driver = driver; }
+    public HomePage(WebDriver driver) { this.driver = driver; }
 
-        public DropdownPage clickDropDown() {
-            clickLink("Dropdown");
-            return new DropdownPage(driver);
-        }
+    public DropdownPage clickDropDown() {
+        clickLink("Dropdown");
+        return new DropdownPage(driver);
+    }
 
-        public ForgotPasswordPage clickForgotPassword() {
-            clickLink("Forgot Password");
-            return new ForgotPasswordPage(driver);
-        }
+    public ForgotPasswordPage clickForgotPassword() {
+        clickLink("Forgot Password");
+        return new ForgotPasswordPage(driver);
+    }
 
-        public LoginPage clickFormAuthentication() {
-            clickLink("Form Authentication");
-            return new LoginPage(driver);
-        }
+    public HoversPage clickHovers() {
+        clickLink("Hovers");
+        return new HoversPage(driver);
+    }
 
-        private void clickLink(String linkText) {
-            driver.findElement(By.linkText(linkText)).click();
-        }
+    public LoginPage clickFormAuthentication() {
+        clickLink("Form Authentication");
+        return new LoginPage(driver);
+    }
+
+    private void clickLink(String linkText) {
+        driver.findElement(By.linkText(linkText)).click();
+    }
 }
