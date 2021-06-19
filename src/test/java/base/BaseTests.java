e15449cb7e4f74cf6af8eed7cf9fc6300d97cc74
Russell Johnson <16086475+RussellJQA@users.noreply.github.com>
2021-06-19 12:47:07 -0400

    Restructure NestedFramesTest to access NestedFrames page through Frames page

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,13 +1,20 @@
 package base;
 
+import com.google.common.io.Files;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
 import utils.WindowManager;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTests {
@@ -38,6 +45,25 @@ public class BaseTests {
     @BeforeMethod
     public void goHome() {
         driver.get("https://the-internet.herokuapp.com/");
+    }
+
+    @AfterMethod
+    public void recordFailure(ITestResult result) {
+        if(ITestResult.FAILURE == result.getStatus()) {
+            var camera = (TakesScreenshot)driver;
+
+            // NOTE: This saves the screenshot into a temporary file that is deleted when the JVM exits.
+            //      See https://www.selenium.dev/selenium/docs/api/java/org/openqa/selenium/OutputType.html
+            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+            System.out.println("Screenshot temporarily saved to: " + screenshot.getAbsolutePath());
+
+            try {
+                // Files.move(screenshot, new File("resources/screenshots/test2.png"));
+                Files.move(screenshot, new File("resources/screenshots/" + result.getName() + ".png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @AfterClass
